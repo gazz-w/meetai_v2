1e47c32a984d05b910cf0b333b40f8c943cdb0b1
Gazz-w <105929513+Gazz-w@users.noreply.github.com>
2024-07-19 12:37:45 -0300

    feature de streaming de audio e gravação adicionadas

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ import streamlit as st
 from dotenv import load_dotenv, find_dotenv
 import openai
 import os
+from streamlit_webrtc import WebRtcMode, webrtc_streamer
+from pathlib import Path
+from datetime import datetime
+import time
+import queue
+import pydub
+
+PASTA_ARQUIVOS = Path(__file__).parent / 'arquivos'
+PASTA_ARQUIVOS.mkdir(exist_ok=True)
 
 # Carregar a API Key do arquivo .env
 load_dotenv()
@@ -9,11 +18,59 @@ api_key = os.getenv("OPENAI_API_KEY")
 
 openai.api_key = api_key
 
+
 # TAB GRAVAR REUNIAO =================
+
+def adiciona_chunk_audio(frames_de_audio, audio_chunk):
+    for frame in frames_de_audio:
+        sound = pydub.AudioSegment(
+            data=frame.to_ndarray().tobytes(),
+            sample_width=frame.format.bytes,
+            frame_rate=frame.sample_rate,
+            channels=len(frame.layout.channels),
+        )
+
+        audio_chunk += sound
+    return audio_chunk
 
 
 def tab_grava_reuniao():
     st.markdown('tab_gravar')
+    webrtx_ctx = webrtc_streamer(
+        key="recebe_audio",
+        mode=WebRtcMode.SENDONLY,
+        audio_receiver_size=1024,
+        media_stream_constraints={"video": False, "audio": True},
+    )
+
+    if not webrtx_ctx.state.playing:
+        st.markdown('Não está rodando')
+        return
+
+    container = st.empty()
+    container.markdown('Comece a falar')
+    pasta_reuniao = PASTA_ARQUIVOS / datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
+    pasta_reuniao.mkdir()
+    audio_chunk = pydub.AudioSegment.empty()
+
+    while True:
+        if webrtx_ctx.audio_receiver:
+            container.markdown('Gravando audio...')
+            try:
+                frames_de_audio = webrtx_ctx.audio_receiver.get_frames(
+                    timeout=1)
+            except queue.Empty:
+                time.sleep(0.1)
+                continue
+
+            audio_chunk = adiciona_chunk_audio(frames_de_audio, audio_chunk)
+
+            if len(audio_chunk) > 0:
+                audio_chunk.export(
+                    pasta_reuniao / 'audio_temp.mp3', format='mp3')
+
+        else:
+            break
 
 
 # TAB SELECAO REUNIAO =================
